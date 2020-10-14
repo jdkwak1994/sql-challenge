b82e607b8031c49025729a21b25712e0d486d2ef
jdkwak1994 <jdkwak1994@gmail.com>
2020-10-14 18:30:32 -0400

    finished product 10/14/20

---
+++ ERD_schema.sql
@@ -0,0 +1,64 @@
+﻿CREATE TABLE Titles (
+    title_id VARCHAR(5)   NOT NULL,
+    title VARCHAR   NOT NULL,
+    CONSTRAINT pk_Titles PRIMARY KEY (
+        title_id
+     )
+);
+
+CREATE TABLE Employees (
+    emp_no INTEGER   NOT NULL,
+    emp_title_id VARCHAR(5)   NOT NULL,
+    birth_date VARCHAR   NOT NULL,
+    first_name VARCHAR   NOT NULL,
+    last_name VARCHAR   NOT NULL,
+    sex VARCHAR(1)   NOT NULL,
+    hire_date VARCHAR   NOT NULL,
+    CONSTRAINT pk_Employees PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE Salaries (
+    emp_no INTEGER   NOT NULL,
+    salary INTEGER   NOT NULL,
+    CONSTRAINT pk_Salaries PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE Department_Employee (
+    emp_no INTEGER   NOT NULL,
+    dept_no VARCHAR(4)   NOT NULL
+);
+
+CREATE TABLE Department_Manager (
+    dept_no VARCHAR(4)   NOT NULL,
+    emp_no INTEGER   NOT NULL
+);
+
+CREATE TABLE Departments (
+    dept_no VARCHAR(4)   NOT NULL,
+    dept_name VARCHAR   NOT NULL,
+    CONSTRAINT pk_Departments PRIMARY KEY (
+        dept_no
+     )
+);
+
+ALTER TABLE Employees ADD CONSTRAINT fk_Employees_emp_title_id FOREIGN KEY(emp_title_id)
+REFERENCES Titles (title_id);
+
+ALTER TABLE Salaries ADD CONSTRAINT fk_Salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employees (emp_no);
+
+ALTER TABLE Department_Employee ADD CONSTRAINT fk_Department_Employee_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employees (emp_no);
+
+ALTER TABLE Department_Employee ADD CONSTRAINT fk_Department_Employee_dept_no FOREIGN KEY(dept_no)
+REFERENCES Departments (dept_no);
+
+ALTER TABLE Department_Manager ADD CONSTRAINT fk_Department_Manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES Departments (dept_no);
+
+ALTER TABLE Department_Manager ADD CONSTRAINT fk_Department_Manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employees (emp_no);
